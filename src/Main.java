a7fc99f1389faec169577d5b5741cb8dd9a6910a
QAVRoman <rvakulka@gmail.com>
2021-12-05 16:09:18 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 5000;
         int bonusMiles = 20;
-        int totalBonusMiles = ticketPrice/bonusMiles;
+        int totalBonusMiles = ticketPrice / bonusMiles;
         System.out.println("Количество бонусных миль = " + totalBonusMiles);
     }
 }
