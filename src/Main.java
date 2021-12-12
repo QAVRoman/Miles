03f7753adbc798fb9fc35b2d125493ffb5e9c5f7
QAVRoman <rvakulka@gmail.com>
2021-12-12 15:49:45 +0300

    Ver. 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 5000;
-        int bonusMiles = 20;
-        int totalBonusMiles = ticketPrice / bonusMiles;
-        System.out.println("Количество бонусных миль = " + totalBonusMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
